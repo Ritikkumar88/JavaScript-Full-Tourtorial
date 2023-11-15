603f14c8e6c0d6159b0d49996e8e35111bcd0fdb
Ritik Shroff <109866300+Ritikkumar88@users.noreply.github.com>
2023-11-15 18:07:56 +0000

    datatype done.

---
+++ 01_basics/04_datatypes_summery.js
@@ -0,0 +1,28 @@
+// datatype is divided into two types on the basis of how it is stored and accessed in the memory , Premitive datatype and nonprimitive datatype;
+
+
+/*
+// ***********premitive datatypes;
+
+7 types (all these are call by values, which means it gets copied not orignal) :
+
+string
+numbers
+bool
+null
+undefined
+symbol (to make any value unique we use symbol)
+bigint
+
+
+// non-premitive datatype(refrance datatypes)**************
+
+// these values are the values whose values can be directly passed by refrence in the memory
+
+arrays
+objects
+functions
+
+*/
+
+
--- 01_basics/conversion_and_opration.js
+++ 01_basics/conversion_and_opration.js
@@ -5,6 +5,17 @@ console.log(typeof (score));
 let valueInNum = Number(score);
 console.log(typeof valueInNum);
 
+// oprations**********
+let value = 3;
+let neg = -value;
+console.log(neg);
+
+// comparison
+console.log("2" > 1);  //output is true even if type is not same
 
 
+
+
+// -----------------------------------
 // node 01_basics/conversion_and_opration
+// -----------------------------------
